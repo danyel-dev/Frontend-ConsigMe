b9056b4574a501b103c3976b0c7fcf5e66f14b29
danyel-dev <dannykill4221x@gmail.com>
2023-11-22 16:50:43 -0300

    a

--- src/ProductDetail/ProductDetail.jsx
+++ src/ProductDetail/ProductDetail.jsx
@@ -1,14 +1,38 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
+
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+
 
 export default function ProductDetail() {
     const { id } = useParams()
+    const [product, setProduct] = useState([])
     
-    axios.get(`http://127.0.0.1:8000/products/${id}/`).then(response => console.log(response.data))
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8000/products/${id}/`).then(response => setProduct(response.data))
+    }, [id]) 
+
     
     return (
         <>
-            <p>oi</p>
-        </>
+            <Header color={"rgb(63, 43, 83)"} />
+            
+            <main>
+                <div className="product">
+                    <img src={product.image} alt={product.name} />
+                    <h3>product.name</h3>
+                    <p>{product.description}</p>
+                    <p>{product.size}</p>
+                </div>
+
+                <div className="comments">
+                    <p>{product.comment_set[0].message}</p>
+                </div>
+            </main>
+
+            <Footer></Footer>
+        </> 
     );
 }
