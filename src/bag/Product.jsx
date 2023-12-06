ccae99b8d7e26e9c7dc02c3399ce0d25716186df
danyel-dev <dannykill4221x@gmail.com>
2023-12-05 23:59:31 -0300

    doing the design of bag product

--- src/bag/Product.jsx
+++ src/bag/Product.jsx
@@ -1,16 +1,23 @@
 export default function Product({product}) {
     return (
-        <div className="flex">
-            <img src={product.image} alt={product.name} className="w-32 h-32" />
+        <div className="flex gap-2">
+            <img src={product.image} alt={product.name} className="w-40 h-40" />
 
-            <div>
-                <h1 className="text-lg">{product.name}</h1>
-                <p className="text-sm">Tam: <span>{product.size}</span></p>
-                <p className="text-sm">Quant: <span>3</span></p>
+            <div className="flex flex-col justify-between gap-2 w-300">
+                <div>
+                    <h1 className="text-lg leading-tight">{product.name}</h1>
+                </div>
 
                 <div>
-                    <h1 className="font-bold">R$ {product.value}</h1>
+                    <small className="block">Tam: <span>{product.size}</span></small>
+                   
+                    <small className="block">Quant: <span>3</span></small>
+                </div>
 
+                <div className="flex justify-between">
+                    <h1 className="font-bold">R$ {product.value}</h1>
+                   
+                    <small className="cursor-pointer bg-red-600 rounded-sm text-white pt-1 pb-1 pl-4 pr-4 font-bold">Deletar</small>
                 </div>
             </div>
         </div>
