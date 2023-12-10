8677a3e270566e2c4186809a41c2bb832b402148
danyel-dev <dannykill4221x@gmail.com>
2023-12-09 22:26:23 -0300

    a

--- src/CreateProfile/CreateProfile.jsx
+++ src/CreateProfile/CreateProfile.jsx
@@ -7,17 +7,32 @@ export default function CreateProfile() {
             <h1>O primeiro passo para se tornar um(a) revendedor(a) na nossa plataforma é criar um perfil de revendedor(a), preencha os campos abaixo e comece seu trabalho ;/</h1>
 
             <form className='create-profile-form'>
-                <input type="text" placeholder='nome da loja' />
-                <input type="email" placeholder='email profissional' />
-                <input type="text" placeholder='Celular' />
-                <input type="text" placeholder='CPF' />
-                <input type="text" placeholder='CEP' />
-                <input type="text" placeholder='Estado' />
-                <input type="text" placeholder='Cidade' />
-                <input type="text" placeholder='Bairro' />
-                <input type="text" placeholder='Rua' />
-                <input type="number" placeholder='Número' />
+                <input type="text" placeholder='Nome da loja' />
+                <input type="email" placeholder='Email profissional' />
+                
+                <div>
+                    <input type="text" placeholder='Celular' />
+                    <input type="text" placeholder='CPF' />
+                </div>
+                
+                <div>
+                    <input type="text" placeholder='CEP' />
+                    <input type="text" placeholder='Estado' />
+                </div>
+                
+                <div>
+                    <input type="text" placeholder='Cidade' />
+                    <input type="text" placeholder='Bairro' />
+                </div>
+                
+                <div>
+                    <input type="text" placeholder='Rua' />
+                    <input type="number" placeholder='Número' />
+                </div>
+                
                 <input type="email" placeholder='Complemento' />
+
+                <button>Enviar</button>
             </form>
         </div>
     );
