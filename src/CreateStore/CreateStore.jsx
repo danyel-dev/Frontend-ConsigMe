b829df5e9d5d22ebf54f5323427ee6d5bf3ebc41
danyel-dev <dannykill4221x@gmail.com>
2024-02-03 10:23:04 -0300

    a

--- src/CreateStore/CreateStore.jsx
+++ src/CreateStore/CreateStore.jsx
@@ -5,15 +5,22 @@ export default function CreateStore() {
     return (
         <div className={styles.container}>
             <div className={styles.sidebar}>
-            
+                <h1>Crie sua conta como lojista no nosso site e venha oferecer seus produtos para sacoleiros(as) autorizados, expanda seus produtos!!</h1> 
+
+                <div>
+                    <p>Caso já tenha uma conta, clique no botão abaixo</p>
+                    <a>Acessar conta</a>
+                </div>
             </div>
 
             <main className={styles.main}>
                 <form className={styles.storeForm}>
                     <input type="text" placeholder="Nome da loja" />
                     <textarea cols="30" rows="4" placeholder="Descrição"></textarea>
-                    <input type="email" placeholder="E-mail" />
-                    <input type="text" placeholder="Número de telefone" />
+                    <div className={styles.doubleInput}>
+                        <input type="email" placeholder="E-mail" />
+                        <input type="text" placeholder="Número de telefone" />
+                    </div>
                     <input type="file" placeholder="Image" />
                     <input type="text" placeholder="CNPJ" />
                     <div className={styles.doubleInput}>
@@ -29,6 +36,8 @@ export default function CreateStore() {
                         <input type="number" placeholder="Número" />
                     </div>
                     <input type="text" placeholder="Complemento" />
+                    
+                    <button>Criar conta</button>
                 </form>
             </main>
         </div>
