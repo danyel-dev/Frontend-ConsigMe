9caa56e5bfdea6ab7952703362f394048360c98c
danyel-dev <dannykill4221x@gmail.com>
2024-02-03 11:29:16 -0300

    a

--- src/CreateStore/CreateStore.jsx
+++ src/CreateStore/CreateStore.jsx
@@ -17,28 +17,39 @@ export default function CreateStore() {
                 <form className={styles.storeForm}>
                     <input type="text" placeholder="Nome da loja" />
                     <textarea cols="30" rows="4" placeholder="Descrição"></textarea>
+
                     <div className={styles.doubleInput}>
                         <input type="email" placeholder="E-mail" />
                         <input type="text" placeholder="Número de telefone" />
                     </div>
+                    
                     <input type="file" placeholder="Image" />
                     <input type="text" placeholder="CNPJ" />
+                    
                     <div className={styles.doubleInput}>
                         <input type="text" placeholder="CEP" />
                         <input type="text" placeholder="Estado" />
                     </div>
+                    
                     <div className={styles.doubleInput}>
                         <input type="text" placeholder="Cidade" />
                         <input type="text" placeholder="Bairro" />
                     </div>
+                    
                     <div className={styles.doubleInput}>
                         <input type="text" placeholder="Rua" />
                         <input type="number" placeholder="Número" />
                     </div>
+                    
                     <input type="text" placeholder="Complemento" />
                     
                     <button>Criar conta</button>
                 </form>
+
+                <div className={styles.haveAccount}>
+                    <p>Caso já tenha uma conta, clique no botão abaixo</p>
+                    <a>Acessar conta</a>
+                </div>
             </main>
         </div>
     );    
