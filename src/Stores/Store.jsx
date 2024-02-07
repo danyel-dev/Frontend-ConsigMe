cb1fdad71a28b3761ff017a6cff1689567dbd54b
danyel-dev <dannykill4221x@gmail.com>
2024-02-07 12:02:36 -0300

    a

--- src/Stores/Store.jsx
+++ src/Stores/Store.jsx
@@ -5,6 +5,16 @@ export default function Store({store}) {
     return(
         <div className={styles.store}>
             <img src={store.image} alt={store.name} />
+            
+            <main className={styles.bodyStore}>
+                <h1>{store.name}</h1>
+                <p className={styles.storeDescription}>{store.description.slice(0, 80)}... <a href="google.com" className={styles.readMoreLink}>ler mais</a></p>
+                <p className={styles.storeAddress}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    {store.city} - {store.state}
+                </p>
+                <a href="google.com" className={styles.storeDetailButton}>Ver detalhes</a>
+            </main>
         </div>
     );
 }
