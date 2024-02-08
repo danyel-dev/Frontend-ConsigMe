1c88977a582b444f6c259e17408b97487d594584
danyel-dev <dannykill4221x@gmail.com>
2024-02-08 12:43:05 -0300

    a

--- src/Stores/Store.jsx
+++ src/Stores/Store.jsx
@@ -4,15 +4,22 @@ import styles from "./stores.module.css";
 export default function Store({store}) {
     return(
         <div className={styles.store}>
-            <img src={store.image} alt={store.name} />
+            <a href=""><img src={store.image} alt={store.name} /></a>
             
             <main className={styles.bodyStore}>
-                <h1>{store.name}</h1>
-                <p className={styles.storeDescription}>{store.description.slice(0, 80)}... <a href="google.com" className={styles.readMoreLink}>ler mais</a></p>
+                <a href=""><h1>{store.name}</h1></a>
+                
+                <p className={styles.storeDescription}>
+                    {store.description.slice(0, 80)}... <a href="google.com" className={styles.readMoreLink}>
+                        ler mais
+                    </a>
+                </p>
+                
                 <p className={styles.storeAddress}>
                     <i className="fa-solid fa-map-pin"></i>
                     {store.city} - {store.state}
                 </p>
+
                 <a href="google.com" className={styles.storeDetailButton}>Ver detalhes</a>
             </main>
         </div>
