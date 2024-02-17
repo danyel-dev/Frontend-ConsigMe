04b43aba484b48e9d29dd28cd152c50835d058bd
danyel-dev <dannykill4221x@gmail.com>
2024-02-17 16:34:00 -0300

    a

--- src/StoreDetail/StoreDetail.jsx
+++ src/StoreDetail/StoreDetail.jsx
@@ -1,9 +1,14 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+import styles from "./storeDetail.module.css"
+
 
 export default function StoreDetail() {
     const { id } = useParams()
+    const [store, setStore] = useState([])
 
     useEffect(() => {
         const config = {
@@ -12,10 +17,20 @@ export default function StoreDetail() {
             }
         }
 
-        axios.get(`http://127.0.0.1:8000/lojistaDetail/${id}`, config).then(response => console.log(response.data)).catch(response => console.log(response))
-    }, [])
+        axios.get(`http://127.0.0.1:8000/lojistaDetail/${id}`, config).then(response => setStore(response.data)).catch(response => console.log(response))
+    }, [id])
 
     return(
-        <p>oi</p>
+        <>
+            <Header />
+            
+            <main className={styles.mainContainer}>
+                <h1>{store.name}</h1>
+                <img src={store.image} alt={store.name} />
+                <p>{store.description}</p>
+            </main>
+
+            <Footer />            
+        </>
     ); 
 }
--- src/Stores/Store.jsx
+++ src/Stores/Store.jsx
@@ -4,13 +4,13 @@ import styles from "./stores.module.css";
 export default function Store({store}) {
     return(
         <div className={styles.store}>
-            <a href=""><img src={store.image} alt={store.name} /></a>
+            <a href={`/stores/${store.id}`}><img src={store.image} alt={store.name} /></a>
             
             <main className={styles.bodyStore}>
-                <a href=""><h1>{store.name}</h1></a>
+                <a href={`/stores/${store.id}`}><h1>{store.name}</h1></a>
                 
                 <p className={styles.storeDescription}>
-                    {store.description.slice(0, 80)}... <a href="google.com" className={styles.readMoreLink}>
+                    {store.description.slice(0, 80)}... <a href={`/stores/${store.id}`} className={styles.readMoreLink}>
                         ler mais
                     </a>
                 </p>
@@ -20,7 +20,7 @@ export default function Store({store}) {
                     {store.city} - {store.state}
                 </p>
 
-                <a href="google.com" className={styles.storeDetailButton}>Ver detalhes</a>
+                <a href={`/stores/${store.id}`} className={styles.storeDetailButton}>Ver detalhes</a>
             </main>
         </div>
     );
